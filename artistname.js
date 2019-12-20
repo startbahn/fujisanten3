0a48a2395936635113bbb2be8a0451c794d07d3a
kubota-stb <noriko.kubota@startbahn.jp>
2019-12-20 13:45:07 +0900

    backup

--- artistname.js
+++ artistname.js
@@ -1,8 +1,8 @@
 //jsonファイルURL
-//let dmain = 'startbahn.org';
-//let id= 'id';
-let file = 'artistname.json';
-//let url = 'https://' + dmain + '/' + id + '/' + file + '.json';
+//var dmain = 'fujisanten.com';
+//var directory = 'json';
+const file = 'artistname.json';
+//var url = 'https://' + dmain + '/' + directory + '/' + file + '.json';
 
 //json読み込み
 fetch(file)
@@ -12,34 +12,21 @@ fetch(file)
   .then(function (json) {
 
     //外枠アクセス
-    let artistName = document.querySelector('.section__about--bgblue');
+    let infoArtist = document.querySelector('.js-about-artist');
 
     //テンプレ複製
     for (let i = 1; i < json.length; i++) {
-      let clone = artistName.firstElementChild.cloneNode(true);
-      artistName.appendChild(clone);
+      let clone = infoArtist.firstElementChild.cloneNode(true);
+      infoArtist.appendChild(clone);
     }
 
-    //artist
-    let artistName = document.querySelectorAll('.js__space-popup--area');
+    //a tag
+    let infoArtistLink = document.querySelectorAll('.js-about-artist-link');
 
-    //Dom出力
+    //Dom
     for (let i = 0; i < json.length; i++) {
-      spaceArea[i].innerHTML = json[i].area;
-      spaceName[i].innerHTML = json[i].name;      
-      spaceAdd[i].innerHTML = json[i].address;
-      spaceWeb[i].href = json[i].web;
-
-      if (label == artist) {
-        artistName.innerHTML = 'あ';
-      }
-
-
-      for (let t = 0; t < json[i].artist.length; t++) {
-        let list = document.createElement('li');     
-        list.innerHTML = '<img src="' + json[i].artist[t].image + '"><div><p>' + json[i].artist[t].name + '</p><p>' + json[i].artist[t].category + '</P></div>';
-        speceArtist[i].appendChild(list);
-      }
+      if (json[i].lavel === artist)
+        infoArtistLink[i].innerHTML = json[i].title;
+      infoArtistLink[i].href = json[i].slug;
     }
-
   });
